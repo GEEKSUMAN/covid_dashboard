a708a845a056627918c352a9374be2b8c229a23d
GEEKSUMAN <IAMSUMAN1808@GMAIL.COM>
2021-11-08 00:43:56 +0530

    remove test

--- test/src/components/homebody/homebody.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const HomeBody = ()=>{
-    return(
-        <div>Homebody for home page</div>
-    )
-}
-
-export default HomeBody
--- test/src/pages/about/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = ()=>{
-    return(
-        <div>About in action</div>
-    )
-}
-
-export default About;
