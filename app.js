c3b6aa88a9e9d8e05bd1e49498f1a77bf764c2ab
GEEKSUMAN <iamsuman1808@gmail.com>
2021-11-07 15:49:51 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const mongoos = require("mongoose")
+const url = 'mongodb://localhost/CovidDB'
+const app = express()
+mongoos.connect(url,{useNewUrlParser:true})
+const con = mongoos.connection
+con.on('open',() => {
+    console.log('Db connected')
+})
+const myRouter = require('./routes/my_routes')
+app.use('/dashboard',myRouter)
+app.listen(9000,()=>{
+console.log('Server running')
+})
